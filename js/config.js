b2c777d2adc6bd35008b10522237d5c49e074302
naappe <naappe@gmail.com>
2026-07-28 17:43:12 +0500

    Add public Supabase configuration

---
+++ js/config.js
@@ -0,0 +1,10 @@
+export const APP_CONFIG = Object.freeze({
+  supabaseUrl: 'https://espezmdpkoixnfchomqb.supabase.co',
+  supabaseKey: 'sb_publishable_xP8z74zcMuCkj6xlu1bJ3w_Kudqbcu1',
+  tables: Object.freeze({
+    supply: 'supply',
+    transactions: 'supply_transactions',
+    vendors: 'supply_vendors'
+  }),
+  pageSize: 25
+});
